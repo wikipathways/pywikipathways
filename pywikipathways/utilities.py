d8c575688d0c6ba929352e3f476413109da33062
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-16 19:27:36 +0900

    Update utilities.py

--- pywikipathways/utilities.py
+++ pywikipathways/utilities.py
@@ -1,4 +1,24 @@
+import urllib.parse
+
 DEFAULT_BASE_URL = 'https://webservice.wikipathways.org'
 
-def wikipathways_get(operation, parameters=NULL, format='json', base_url=DEFAULT_BASE_URL):
-  pass
+def wikipathways_get(operation, parameters=None, format='json', base_url=DEFAULT_BASE_URL):
+    try:
+        url = build_url(base_url, operation)
+        r = requests.request('GET', url, params=parameters)
+        r.raise_for_status()
+        try:
+            return r.json()
+        except ValueError as e:
+            if require_json:
+                raise
+            else:
+                return r.text
+    except requests.exceptions.RequestException as e:
+        _handle_error(e)
+
+def build_url(base_url=DEFAULT_BASE_URL, operation=None):
+    if operation is not None:
+        return base_url + "/" + urllib.parse.quote(operation)
+    else:
+        return base_url
