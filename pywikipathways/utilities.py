07a0b35938a43f44c90f9821d897073510b150e5
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-16 20:18:04 +0900

    Update utilities.py

--- pywikipathways/utilities.py
+++ pywikipathways/utilities.py
@@ -1,8 +1,25 @@
+import requests
 import urllib.parse
 
 DEFAULT_BASE_URL = 'https://webservice.wikipathways.org'
 
-def wikipathways_get(operation, parameters=None, format='json', base_url=DEFAULT_BASE_URL):
+def wikipathways_get(operation, parameters=None, base_url=DEFAULT_BASE_URL):
+    """WikiPathways GET.
+    
+    FILLME
+    FILLME
+    
+    Args:
+        operation (str): operation
+        base_url (str): Ignore unless you need to specify a custom domain
+    
+    Returns:
+        str: query result content
+    
+    Examples:
+        >>> wikipathways_get('listOrganisms')
+        >>> wikipathways_get('listOrganisms', parameters={'format': 'json'})
+    """
     try:
         url = build_url(base_url, operation)
         r = requests.request('GET', url, params=parameters)
@@ -10,10 +27,7 @@ def wikipathways_get(operation, parameters=None, format='json', base_url=DEFAULT
         try:
             return r.json()
         except ValueError as e:
-            if require_json:
-                raise
-            else:
-                return r.text
+            return r.text
     except requests.exceptions.RequestException as e:
         _handle_error(e)
 
