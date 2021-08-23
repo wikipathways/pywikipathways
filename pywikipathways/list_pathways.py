c64435a74a7abb41f083263eabe55bc2ec5cd8b8
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-24 01:57:05 +0900

    Update list_pathways.py

--- pywikipathways/list_pathways.py
+++ pywikipathways/list_pathways.py
@@ -1,16 +1,8 @@
-def list_pathways(organism=""):
-    res = wikipathways_get('listpathways', {'organism' = organism})
-    pass
+import pandas
 
-# listPathways <- function(organism="") {
-#     res <- wikipathwaysGET('listPathways', list(organism=organism))
-#     if(length(res$pathways) == 0){
-#         message("No results")
-#         return(data.frame())
-#     }
-#     #make into list of list (like other web service responses)
-#     res.pathways <- lapply(res$pathways, as.list)
-#     res.df <- suppressWarnings(data.table::rbindlist(res.pathways, fill = TRUE))
-#     res.df$revision <- vapply(res.df$revision, as.integer, integer(1)) 
-#     return(res.df)
-# } 
+def list_pathways(organism=""):
+    res = wikipathways_get('listpathways', {'organism' = organism, 'format': 'json'})
+    if 'pathways' in res.keys():
+        return(pandas.DataFrame(res['pathways']))
+    else:
+        print("No results")
