2f627cf2d335849ae2ccf80349bcd3db2615f28e
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-25 05:21:12 +0900

    Update find_pathway_by_text.py

--- pywikipathways/find_pathway_by_text.py
+++ pywikipathways/find_pathway_by_text.py
@@ -6,3 +6,15 @@ def find_pathways_by_text(query):
     score = tmp['score'].apply(lambda x: x['0'])
     tmp['score'] = score
     return tmp.drop(['fields'], axis=1).drop_duplicates(ignore_index=True)
+
+def find_pathway_ids_by_text(query):
+    res = find_pathways_by_text(query)
+    return res['id']
+
+def find_pathway_names_by_text(query):
+    res = find_pathways_by_text(query)
+    return res['name']
+
+def find_pathway_urls_by_text(query):
+    res = find_pathways_by_text(query)
+    return res['url']
