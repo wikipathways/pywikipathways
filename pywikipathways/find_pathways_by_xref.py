6ffa3ce38e194dcdf710691163717618b72405d7
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-25 02:15:45 +0900

    Update find_pathways_by_xref.py

--- pywikipathways/find_pathways_by_xref.py
+++ pywikipathways/find_pathways_by_xref.py
@@ -7,3 +7,15 @@ def find_pathways_by_xref(identifier, systemCode):
     score = tmp['score'].apply(lambda x: list(x.values())[0])
     tmp['score'] = score
     return tmp.drop(['fields'], axis=1).drop_duplicates(ignore_index=True)
+
+def find_pathway_ids_by_xref(identifier, systemCode):
+    res = find_pathways_by_xref(identifier, systemCode)
+    return res['id']
+
+def find_pathway_names_by_xref(identifier, systemCode):
+    res = find_pathways_by_xref(identifier, systemCode)
+    return res['name']
+
+def find_pathway_urls_by_xref(identifier, systemCode):
+    res = find_pathways_by_xref(identifier, systemCode)
+    return res['url']
