6d8e26df913c5238ce66290d56cff576aea8655d
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-25 05:10:27 +0900

    Update find_pathways_by_xref.py

--- pywikipathways/find_pathways_by_xref.py
+++ pywikipathways/find_pathways_by_xref.py
@@ -3,8 +3,8 @@ import pandas
 def find_pathways_by_xref(identifier, systemCode):
     res = wikipathways_get('findPathwaysByXref', {'ids': identifier, 'codes': systemCode, 'format': 'json'})
     tmp = pandas.DataFrame(res['result'])
-    # score = tmp['score'].apply(lambda x: x['0'])
-    score = tmp['score'].apply(lambda x: list(x.values())[0])
+    score = tmp['score'].apply(lambda x: x['0'])
+    # score = tmp['score'].apply(lambda x: list(x.values())[0])
     tmp['score'] = score
     return tmp.drop(['fields'], axis=1).drop_duplicates(ignore_index=True)
 
