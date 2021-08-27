0ef7135ad6e81a0966c6d22d4750c90f21b4e758
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-28 00:16:33 +0900

    Update list_pathways.py

--- pywikipathways/list_pathways.py
+++ pywikipathways/list_pathways.py
@@ -1,6 +1,21 @@
 from .utilities import *
 
 def list_pathways(organism=""):
+    """List Pathways
+    
+    Retrieve list of pathways per species, including WPID, name,
+    species, URL and latest revision number.
+    
+    Args:
+        organism (str): A particular species.
+    
+    Returns:
+        dataframe: A dataframe of pathway information.
+        
+    Examples:
+        >>> list_pathways('Mus musculus')
+    
+    """
     res = wikipathways_get('listpathways', {'organism':  organism, 'format': 'json'})
     if 'pathways' in res.keys():
         return pandas.DataFrame(res['pathways'])
