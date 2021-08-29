60e4a6346099b34f4e62a0e3248ff0289bbea43e
Kozo Nishida <kozo.nishida@gmail.com>
2021-08-30 01:32:11 +0900

    Update download_pathway_archive.py

--- pywikipathways/download_pathway_archive.py
+++ pywikipathways/download_pathway_archive.py
@@ -26,7 +26,8 @@ def download_pathway_archive(date='current', organism=None, format='gpml', destp
         if date == 'current':
             curr_files = pandas.read_html("https://wikipathways-data.wmcloud.org/current/" + format)[0]['Filename']
             filename = curr_files[curr_files.str.contains(organism.replace(" ", "_"))]
-            if not True in curr_files.str.cotains(organism.replace(" ", "_")):
+            filename = list(filename)[0]
+            if not True in curr_files.str.contains(organism.replace(" ", "_")):
                 sys.exit('Could not find a file matching your specifications. Try browsing http://data.wikipathways.org.')
         else:
             if requests.get("https://wikipathways-data.wmcloud.org/" + date).ok:
