4658166867587ab4cbaadfe2b9f639e0bfaf5cf8
Kozo Nishida <kozo.nishida@gmail.com>
2023-08-19 11:27:17 +0900

    Add test_find_pathways_by_text()

---
+++ tests/test_find_pathway_by_text.py
@@ -0,0 +1,14 @@
+#from find_pathway_by_text import find_pathways_by_text
+import pytest
+from pywikipathways.find_pathway_by_text import *
+
+def test_find_pathways_by_text():
+    # Test with a query that should return at least one pathway
+    query = 'cancer'
+    result = find_pathways_by_text(query)
+    assert len(result) > 0
+
+    # Test with a query that should not return any pathways
+    query = 'qwertyuiop'
+    result = find_pathways_by_text(query)
+    assert len(result) == 0
