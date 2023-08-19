2187c0f1c51f3fcf9bbed7bc4175b7978d4cacfb
Kozo Nishida <kozo.nishida@gmail.com>
2023-08-19 13:00:25 +0900

    Add test_find_pathways_by_xref

---
+++ tests/test_find_pathways_by_xref.py
@@ -0,0 +1,11 @@
+import pytest
+from pywikipathways.find_pathways_by_xref import *
+
+def test_find_pathways_by_xref():
+    # find by Xref
+    pathways = find_pathways_by_xref('ENSG00000232810','En')
+    assert len(pathways) > 0
+
+    # find by Xref (again)
+    pathways = find_pathways_by_xref(identifier="ENSG00000232810", systemCode="En")
+    assert len(pathways) > 0
