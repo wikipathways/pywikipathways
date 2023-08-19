18e20bb3808a7a80ab7cfcbcaa3116e767b7dc77
Kozo Nishida <kozo.nishida@gmail.com>
2023-08-19 12:11:45 +0900

    Add try-except

--- pywikipathways/find_pathway_by_text.py
+++ pywikipathways/find_pathway_by_text.py
@@ -2,10 +2,13 @@ from .utilities import *
 
 def find_pathways_by_text(query):
     res = wikipathways_get('findPathwaysByText', {'query': query, 'format': 'json'})
-    tmp = pandas.DataFrame(res['result'])
-    score = tmp['score'].apply(lambda x: x['0'])
-    tmp['score'] = score
-    return tmp.drop(['fields'], axis=1).drop_duplicates(ignore_index=True)
+    tmp = pandas.DataFrame(res['result'])    
+    try:
+        score = tmp['score'].apply(lambda x: x['0'])
+        tmp['score'] = score
+        return tmp.drop(['fields'], axis=1).drop_duplicates(ignore_index=True)
+    except KeyError:
+        print("No results")
 
 def find_pathway_ids_by_text(query):
     res = find_pathways_by_text(query)
